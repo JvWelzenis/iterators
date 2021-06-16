9c43dc680a409492b119bb7084c99ccc6b741aef
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-06-16 20:41:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/*var colors = ["yellow", "red", "blue", "orange"];
+var i = 0;
+var text = "";
+
+while (colors[i]) {
+    text += colors[i] + "<br>";
+    i++;
+}
+
+console.log(text); 
+
+var colors = ["yellow", "red", "blue", "orange"];
+var i = 0;
+var text = "";
+
+for (i = 0; i < colors.length; i++) {
+    text += colors[i] + "<br>";
+  }
+
+  console.log(text); */
+
+  
